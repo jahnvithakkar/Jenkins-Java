0c0405f24df4eceaf31b0284358c5c6d776b5997
jahnvithakkar <78201928+jahnvithakkar@users.noreply.github.com>
2022-02-22 14:50:49 +0530

    Update NewJavaProgram.java

--- NewJavaProgram.java
+++ NewJavaProgram.java
@@ -1,7 +1,7 @@
-public class hello{
+public class NewJavaProgram{
     public static void main(String[] args){
-        for(i=1;i<=5;i++){
-            System.out.println("HELLO WORLD! "+i)
+        for(int i=1;i<=5;i++){
+            System.out.println("HELLO WORLD! "+i);
         }
     }
 }
